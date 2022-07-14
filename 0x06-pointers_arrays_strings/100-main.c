91ed864ef781970a656f3ba4350edf1970e3b399
dereje01 <derejemesfin3@gmail.com>
2022-07-14 11:32:06 -0700

    8. rot13

--- 0x06-pointers_arrays_strings/100-main.c
+++ 0x06-pointers_arrays_strings/100-main.c
@@ -8,8 +8,9 @@
  */
 int main(void)
 {
-    char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
-    char *p;
+
+	char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
+    	char *p;
 
     p = rot13(s);
     printf("%s", p);
